a1e047f47bf2b4ed3e65c2eddebdcd4a2003a614
Fuad Hasan <fuadhasan693056@gmail.com>
2025-03-10 21:50:05 +0600

    using loops for program

---
+++ loop.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //operators
+    // logical operators
+    // && - and ( both conditions must be true. 1 < 5 && 5 < 10)
+    // || - or (either condition must be true. 1 < 5 || 5 < 10)
+    // ! - not (1 < 5) is false, but !(1 < 5) is true
+
+    // compersition operators
+    // == - equal to
+    // != - not equal to
+    // > - greater than
+    // < - less than
+    // >= - greater than or equal to
+    // <= - less than or equal to
+
+    // loop
+    // int i = 0;
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j<i; j++){
+            cout << "* ";
+        }
+        cout<< endl;
+    }
+    // cout << !(1 < 5) << endl;
+    return 0;
+}
